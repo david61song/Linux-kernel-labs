8d4d87698ac909f65ea308dbc5f718f94bca22e8
david61song <david61song@protonmail.com>
2024-07-31 15:37:55 +0900

    Character device updated
    1. All function implemented (Not yet tested)

--- kernel-utils/labs/lab11/my_cdev.c
+++ kernel-utils/labs/lab11/my_cdev.c
@@ -4,16 +4,15 @@
  * All tasks
  *
  * Skeleton developed by UPB, modified for Ajou Univ. SCE394.
- *
  * Compeleted by Eunseok Song, (@david61song)
  *
  */
-
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/cdev.h>
-/* add header files to complete thie module */
+#include <asm/uaccess.h>
+#include <asm/ioctl.h>
 
 #include "my_cdev.h"
 
@@ -30,36 +29,46 @@ MODULE_LICENSE("GPL");
 /* Device identification number */
 #define MY_MAJOR 42
 #define MY_MAX_MINORS 5
+#define FIRST_MINOR_NUM 0
+
 
 
 struct my_device_data {
 	struct cdev cdev; /* cdev structure data */
 	char device_buffer[BUFSIZ]; /* our device buffer */
 	atomic_t access; /* atomic_t access variable size to track if file is opened */
+	size_t size;
 };
 
-static size_t my_min(size_t n1, size_t n2) {
-	if (n1 <= n2)
-		return n1;
-	else
-		return n2;
-}
+/* array of device data */
+/* we define my_device_data array that holds device data which have size FIRST_MINOR_NUM 
+ * That is, we assume that we have MY_MAX_MINORS number of devices. */
+
+struct my_device_data devs[MY_MAX_MINORS]; 
+
 
 static int my_open(struct inode *inode, struct file *file)
 {
+	struct my_device_data *my_data;
+	/* print message when the device file is open. */
 
-	/* TODO: print message when the device file is open. */
+	/* inode->i_cdev contains our cdev struct, use container_of to obtain a pointer to my_device_data */
+	/* The purpose of the container_of macro is to find the starting address of a structure
+	 * when the address of one of its members is known.
+	*/
 
-	/* TODO: inode->i_cdev contains our cdev struct, use container_of to obtain a pointer to my_device_data */
+	my_data = container_of(inode->i_cdev, struct my_device_data, cdev);
+	file->private_data = my_data;
 
-	/* TODO: return immediately if access is != 0, use atomic_cmpxchg */
+	/* return immediately if access is != 0, use atomic_cmpxchg */
 	if(atomic_cmpxchg(&my_data->access, 0, 1) != 0)
 		return -EBUSY;
-
+	printk("Device opened");
 	/*
 	 *  If you want to test open after relase device file, 
 	 *  use below functions to sleep open().
 	 */
+
 	// set_current_state(TASK_INTERRUPTIBLE);
 	// schedule_timeout(10 * HZ);
 
@@ -70,10 +79,10 @@ static int my_release(struct inode *inode, struct file *file)
 {
 	struct my_device_data *my_data =
 		(struct my_device_data *) file->private_data;
-	/* TODO: print message when the device file is closed. */
 
-	/* TODO: reset acces varable to 0, use atomic_set */
 	atomic_set(&my_data->access, 0);
+
+	printk("Device closed (released)");
 
 	return 0;
 }
@@ -82,9 +91,16 @@ static ssize_t my_read(struct file *file,
 		char __user *user_buffer,
 		size_t size, loff_t *offset)
 {
-	size_t len;
+	struct my_device_data *my_data = (struct my_device_data *) file->private_data;
+	ssize_t len = min(my_data->size - *offset, size);
 
-	/* TODO: Copy my_data->buffer to user_buffer, use copy_to_user */
+	if (len <= 0)
+	    return 0;
+
+	if (copy_to_user(user_buffer, my_data->device_buffer + *offset, len))
+		return -EFAULT;
+
+	*offset += len;
 
 	return len;
 }
@@ -93,9 +109,19 @@ static ssize_t my_write(struct file *file,
 		const char __user *user_buffer,
 		size_t size, loff_t *offset)
 {
-	/* TODO: copy user_buffer to my_data->buffer, use copy_from_user */
+    struct my_device_data *my_data = (struct my_device_data *) file->private_data;
+    size_t len = min(my_data->size - *offset, size);
 
-	return size;
+    if (len <= 0)
+	return 0;
+
+    if (copy_from_user(my_data->device_buffer + *offset, user_buffer, len))
+	return -EFAULT;
+
+    *offset += len;
+
+    return len;
+
 }
 
 static long my_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
@@ -105,9 +131,24 @@ static long my_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	int remains;
 
 	switch (cmd) {
-		/* TODO: if cmd = MY_IOCTL_PRINT, display IOCTL_MESSAGE */
-		/* TODO: if cmd = MY_IOCTL_SET_BUFFER, write arg to my_data->buffer */
-		/* TODO: if cmd = MY_IOCTL_GET_BUFFER, read my_data->buffer to arg */
+	    /* if cmd = MY_IOCTL_PRINT, display IOCTL_MESSAGE */
+	    case MY_IOCTL_PRINT:
+		printk(KERN_INFO "%s\n", IOCTL_MESSAGE);
+		break;
+	    /* if cmd = MY_IOCTL_GET_BUFFER, read my_data->buffer to arg */
+	    case MY_IOCTL_GET_BUFFER:
+		if (copy_to_user((char __user *)arg, my_data->device_buffer, my_data->size))
+		    ret = -EFAULT;
+		break;
+	    /* if cmd = MY_IOCTL_SET_BUFFER, write arg to my_data->buffer */
+	    case MY_IOCTL_SET_BUFFER:
+		remains = copy_from_user(my_data->device_buffer, (char __user *)arg, BUFFER_SIZE);
+
+		if (remains)
+		    ret = -EFAULT;
+		my_data->size = BUFFER_SIZE - remains;
+		break;
+	    /* Unknown command */
 		default:
 			ret = -EINVAL;
 	}
@@ -117,10 +158,11 @@ static long my_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 
 static const struct file_operations my_fops = {
 	.owner = THIS_MODULE,
-	/* TODO: add open and release functions */
-	/* TODO: add read function */
-	/* TODO: add write function */
-	/* TODO: add ioctl function */
+	.open = my_open,
+	.read = my_read,
+	.write = my_write,
+	.release = my_release,
+	.unlocked_ioctl = my_ioctl
 };
 
 static int __init my_init_module(void)
@@ -129,15 +171,23 @@ static int __init my_init_module(void)
 	int i;
 
 	/* register char device region for MY_MAJOR and MY_MAX_MINORS starting at 0 */
+	/* reserves it */
+	err = register_chrdev_region(MKDEV(MY_MAJOR, FIRST_MINOR_NUM), MY_MAX_MINORS, "mydev");
 
-	err = register_chrdev_region((MKDEV(MY_MAJOR), 0), MY_MAX_MINORS, "mydev");
-	
+	if (err != 0){
+	    return err;
+	}
 
 	for (i = 0; i < MY_MAX_MINORS; i++) {
-		/* TODO: init and add cdev to kernel core */
-		/* TODO: set access variable to 0, use atomic_set */
+		/* character device will have to be initialized */
+		cdev_init(&devs[i].cdev, &my_fops);
+		/* kernel will have to be notified */
+		cdev_add(&devs[i].cdev, MKDEV(MY_MAJOR, i), 1); 
+		/* atomic varaible set */
 		atomic_set(&devs[i].access, 0);
-		/* TODO: initialize buffer with MESSAGE string */
+		/* size of transfer data size */
+		devs[i].size = BUFSIZ / 2;
+
 	}
 
 	return 0;
@@ -145,8 +195,15 @@ static int __init my_init_module(void)
 
 static void __exit my_cleanup_module(void)
 {
-	/* TODO: delete cdev from kernel core */
-	/* TODO: unregister char device region, for MY_MAJOR and MY_MAX_MINORS starting at 0 */
+    int i;
+
+    /* delete cdev from kernel core */
+
+    for (i = 0 ; i < MY_MAX_MINORS ; i++){
+    /* unregister char device region, for MY_MAJOR and MY_MAX_MINORS starting at 0 */
+	cdev_del(&devs[i].cdev);
+    }
+    unregister_chrdev_region(MKDEV(MY_MAJOR,0), MY_MAX_MINORS);
 }
 
 module_init(my_init_module);
--- kernel-utils/labs/lab11/my_cdev.h
+++ kernel-utils/labs/lab11/my_cdev.h
@@ -9,4 +9,12 @@
 #define MESSAGE			"hello\n"
 #define IOCTL_MESSAGE		"Hello ioctl"
 
+/* for ioctl */
+
+#define MY_IOCTL_PRINT		_IOC(_IOC_NONE, 'k', 1, 0)
+#define MY_IOCTL_SET_BUFFER	_IOC(_IOC_WRITE, 'k', 2, BUFFER_SIZE)
+#define MY_IOCTL_GET_BUFFER	_IOC(_IOC_READ, 'k', 3, BUFFER_SIZE)
+
+
+
 #endif
